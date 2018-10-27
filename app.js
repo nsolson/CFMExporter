1448d8648e4da05d236b93ac4abc021d1407d289
nsolson <olson.nate.s@gmail.com>
2018-10-27 00:14:38 -0600

    Update app.js with firebase info

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./maddenexporterbk-firebase-adminsdk-egj75-d36c518d4b.json");
+const serviceAccount = require("./madden-cfm-nsolson.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://maddenexporterbk.firebaseio.com"
+  databaseURL: "https://madden-cfm-f7700.firebaseio.com"
 });
 
 // Setup
