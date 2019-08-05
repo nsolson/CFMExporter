e88d59288d0b1098f3a2c4432c3310ee7763cf05
Olson <nolson@mgc.mentorg.com>
2019-08-05 10:27:57 -0600

    testing with real stuff
    testing with temp private key

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ const app = express();
 admin.initializeApp({
 	credential: admin.credential.cert({
 		"type": "service_account",
-		"project_id": process.env.FIREBASE_PROJECT_ID,
-		"private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-		"private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-		"client_email": process.env.FIREBASE_CLIENT_EMAIL,
-		"client_id": process.env.FIREBASE_CLIENT_ID,
+		"project_id": "madden-cfm-f7700",
+		"private_key_id": "25cb992b2b24274ad9a991e50950f64b0f4b3542",
+		"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDOttA44lU0vSt0\nN+b6mHDrxjlwDM8k1398rhthitCSWEOAG4Xi8QfZGjutXgfFBO/rPmeVSontgwO3\ntn5UhbqgovZEB13FZMalWqVk7j/TNjB4meOhEbEauGlnDIXTA9EgdE5CiSzfDUg6\n0IGg5wC3NC8W8NzE5iW6qjJZl6fz5fFfvpUDdZBy/O/DmAW606BlicFV0+8BJDj5\nm81GM/a1su2aIsMC3Og0OofaKtuxDoi9wa/aArJlR86r948xw7heCXGAiXp/BysF\nJAww7prCWUPisBtvrjuiI1ppd1SbvGEus/4/Rqm2CChYhLQuJMgdNPzTOH7oMHan\nOwizr1VtAgMBAAECggEAMTVBshjm8awKmRrpqEP7tZS1fZ/++6U1GwgTSpASAJR0\nVR24LIL2PE9LX4Tf0p3PQy/S1q4s0VZ73xLZVeOTvsOsXhtmziOkSh6e+rzggrYG\nqd7wWhptD5BQvxAzFz10XgZxztL61IpD5IRYJ2Z2PCZ3Az4JP/INjzcOz1cTyzJG\nx/uReJLB39ydfpMgX9h5WmjtFxUtdjrcvpLk6emefv2dNs9RSDdT5RXqR/2iBmZU\nF9N+QjUPH0DAYfpJrKrkFDlA6iK8mO10/lp2RvgpthKiSTUYBx1cSpNipe3j+0nN\nHqQCzcR8LiDYrjRm7GnzAZ1nsxK4M4RmaSKBDMNmIQKBgQD+D0Cvl4MI826dbdgy\n9OgYJcBemQlHK2fQI4VgkGRTj8czJytDuUdMcr8IORAfFbjvvTeFEbEtBvmHMEUu\nIc49C4E30G8NIXh21fdby8TqXyFSB/YCgvqnJ18iyDGgWW77426jOvtgTAQgmqcG\nPZe1Il4M2aJe672gFCWI8vSsxQKBgQDQSv0syHE726tPkzly/jj2yJG3q96NEJcp\nOh/A5IDAZU7JjhUdRDmZpgyhx0YOiZLk9TD7UMngiGXAGGTJJYtNSu3gyviHuWHo\nc2FRxcoYH2j4NRYRLRGEGq3yQ1qDMUofoTQq9TWUCHqfaT4IPhYuvCZR6eRxCzDK\n1Uvor3FgiQKBgQCz5I70Bn91JhGsDWxp4Ho7JvgIw6oLJ9PxUEWP843mkUcGlHSR\nFq9ClXlo1CbzK8lwj/WxFq7y6B5luCs0GCHVncDYiZEopguLccQdzrJXFhcc46Ax\ndRcQptM5aLGjDdCFMSxVDl2SeSDvCTK9O32hd31K8guD1L7Hi5xsVUQk1QKBgBU+\nUKvslX/hW4t2HHnZ671fn1z4Lm58rq4Ju4tUwrzcmDh+QTEG8Ej3YnptdHuvFEg9\nqY04JB6+SoJY+o55QcfUSIaQrOBKy+2FNvu8DGyBZ5dI8sM1sX+jqPH4ZJbUxBaN\nvmBTOe+rHbZ8aVM14dVKYy4fiKju1z8YmfFVU17hAoGBAPKDlBxhCcawLusza4qj\np4PIxJYxVFQwS/QuWjpJgnYbuTHpi3Ls4qhQbwRQubdNaFCCyg7/n+4hPSsKWELG\npVGK3vLhpu9lFk5jwVrVB3gYRkD70nvf8BHFfZ4bw0c38BaVHsEbINlsJytfWqad\n4MG0gXdo28nW7bsPV8FIidTI\n-----END PRIVATE KEY-----\n",
+		"client_email": "firebase-adminsdk-atakm@madden-cfm-f7700.iam.gserviceaccount.com",
+		"client_id": "117216417204021598373",
 		"auth_uri": "https://accounts.google.com/o/oauth2/auth",
 		"token_uri": "https://oauth2.googleapis.com/token",
 		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-		"client_x509_cert_url": process.env.FIREBASE_CERT_URL
+		"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-atakm%40madden-cfm-f7700.iam.gserviceaccount.com"
 	}),
-	databaseURL: "https://" + process.env.FIREBASE_PROJECT_ID + ".firebaseio.com"
+	databaseURL: "https://madden-cfm-f7700.firebaseio.com"
 });
 
 
